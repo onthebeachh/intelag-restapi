45ea31eecaf67129ec293807dfbf005296f96efa
Sergio Esteban <sergio.cardenas@radeonlabs.com>
2016-01-27 17:02:36 -0300

    hot fix

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import os
+try:
+  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+  from SocketServer import TCPServer as Server
+except ImportError:
+  from http.server import SimpleHTTPRequestHandler as Handler
+  from http.server import HTTPServer as Server
+
 #!flask/bin/python
 from flask import Flask, jsonify
 
@@ -75,6 +83,20 @@ data = [ {
     }
   }
 ]
+
+
+# Read port selected by the cloud for our application
+PORT = int(os.getenv('PORT', 8000))
+# Change current directory to avoid exposure of control files
+os.chdir('static')
+
+httpd = Server(("", PORT), Handler)
+try:
+  print("Start serving at port %i" % PORT)
+  httpd.serve_forever()
+except KeyboardInterrupt:
+  pass
+httpd.server_close()
 
 @app.route('/recipes', methods=['GET'])
 def get_tasks():
